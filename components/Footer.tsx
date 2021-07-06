414a0fc8cf5179daf9ade149c496c5bee1004057
jussmor <jussmor@outlook.es>
2021-07-05 19:37:05 -0500

    footer terminado

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,27 +1,35 @@
-import gsap from "gsap"
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
-import styles from "../styles/Footer.module.css"
+import Image from "next/image";
+import styles from "../styles/Footer.module.css";
 
 export default function Footer() {
-  
+  let date  = new Date();
+  let year =  date.getUTCFullYear();
   return (
     <div>
-        <div className={styles.wrapFooter}>
-              <div className={styles.mytitle}>
-                  JUSSMOR
-              </div>
-              <nav className={styles.navFooter}>
-                  <div> my imagen</div>
-                  <div>
-                    <ul className={styles.navFooterItems}>
-                        <li>Ig</li>
-                        <li>Yt</li>
-                        <li>TK</li>
-                    </ul>
-                  </div>
-              </nav>
-              <div> copyright</div>
+      <div className={styles.wrapFooter}>
+        <div className={styles.mytitle}>JUSSMOR</div>
+        <nav className={styles.navFooter}>
+          <div>  </div>
+          <div>
+            <div className={styles.navFooterItems}>
+              <a href="https://www.instagram.com/jussmor" className={styles.LogoItems}>
+                <img  src="/svg/instagram-brands.svg" alt="Instagram" />
+              </a>
+              <a  href="https://www.youtube.com/channel/UCXHTBH56s3VKSTHdLYupAlg" className={styles.LogoItems}>
+                <img  src="/svg/youtube-brands.svg" alt="Youtube" />
+              </a>
+              <a href="https://www.tiktok.com/@jussmor" className={styles.LogoItems}>
+                <img  src="/svg/tiktok-brands.svg" alt="TikTok" />
+              </a>
+            </div>
+          </div>
+        </nav>
+        <div className={styles.Copyright}> 
+          <p className={styles.CopyrightChild}> &copy; { year } Copyright Jussmor</p>
         </div>
+      </div>
     </div>
   );
 }
