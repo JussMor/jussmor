075d8e063cd2963cfe85c41da1479eefdfa6402b
Junior Moreira <jussmor@outlook.es>
2024-03-15 11:13:19 -0500

    feat: more changes to navbar

--- apps/website.ui/src/routes/layout.tsx
+++ apps/website.ui/src/routes/layout.tsx
@@ -30,27 +30,35 @@ export default component$(() => {
 
   return (
     <>
-      <Navbar class='flex gap-3 justify-center px-8 py-3'>
-            {links.map(({name, link, sublink}, index)=> (
-              <div
-              onMouseEnter$={()=> activeNav.value = index}
-              onMouseLeave$={() => activeNav.value = undefined}
-              >
-                <NavbarLinks key={`navbar-${index}`} class='text-sm'>
-                    {name}
-                </NavbarLinks>
-              </div>
-            ))}
-      </Navbar>
-      {activeNav.value !== undefined && links[activeNav.value]?.sublink && ( // Check if activeNav is defined and within bounds
-        <div>
-          {links[activeNav.value].sublink?.map((sublink, subIndex) => (
-            <div key={`sublink-${subIndex}`}>
-              <a href={sublink.link}>{sublink.name}</a>
+    <Navbar class='fixed w-full z-40  '>
+        <div class='relative z-50'>
+            <div class='flex gap-3 justify-center px-8 py-3 bg-background'>
+                {links.map(({name, link, sublink}, index)=> (
+                    <div
+                        onMouseEnter$={()=> activeNav.value = index}
+                        onMouseLeave$={() => activeNav.value = undefined}
+                    >
+                        <NavbarLinks key={`navbar-${index}`} class='text-sm'>
+                            {name}
+                        </NavbarLinks>
+                    </div>
+                ))}
             </div>
-          ))}
         </div>
-      )}
+        <div class='relative z-20'>
+            <div class='absolute bg-foreground w-full transition-all duration-300 ease-in-out top-0 transform translate-y-full opacity-0' style={{ opacity: activeNav.value !== undefined ? 1 : 0, transform: activeNav.value !== undefined ? 'translateY(0)' : 'translateY(-100%)' }}>
+                <Container>
+                    {activeNav.value !== undefined && links[activeNav.value]?.sublink && (<>
+                        {links[activeNav.value].sublink?.map((sublink, subIndex) => (
+                            <div key={`sublink-${subIndex}`}>
+                                <a class='text-background'  href={sublink.link}>{sublink.name}</a>
+                            </div> 
+                        ))}
+                    </>)}
+                </Container>
+            </div>
+        </div>
+    </Navbar>
       <main  >
         <section>
           <Slot />
